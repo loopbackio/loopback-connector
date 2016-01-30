70e577d52a6b56ae00b5be3fe02fdd2d12f7cfd4
Simon Ho <simonho@ca.ibm.com>
2016-01-29 17:17:52 -0800

    lint transient.test.js

--- test/transient.test.js
+++ test/transient.test.js
@@ -1,38 +1,34 @@
-var jdb = require('../');
-var DataSource = jdb.DataSource;
-var assert = require('assert');
-var async = require('async');
+var DataSource = require('..').DataSource;
 var should = require('./init.js');
 
-var db, TransientModel, Person, Widget, Item;
+describe('connectors.transient', function() {
+  var db;
+  var Item;
+  var Person;
+  var TransientModel;
+  var Widget;
 
-var getTransientDataSource = function(settings) {
-    return new DataSource('transient', settings);
-};
+  before(function() {
+    db = new DataSource('transient');
+    TransientModel = db.define('TransientModel', {}, {idInjection: false});
 
-describe('Transient connector', function () {
-  
-  before(function () {
-    db = getTransientDataSource();
-    TransientModel = db.define('TransientModel', {}, { idInjection: false });
-    
     Person = TransientModel.extend('Person', {name: String});
     Person.attachTo(db);
-    
+
     Widget = db.define('Widget', {name: String});
     Item = db.define('Item', {
       id: {type: Number, id: true}, name: String
     });
   });
-  
+
   it('should respect idInjection being false', function(done) {
     should.not.exist(Person.definition.properties.id);
     should.exist(Person.definition.properties.name);
-    
-    Person.create({ name: 'Wilma' }, function(err, inst) {
+
+    Person.create({name: 'Wilma'}, function(err, inst) {
       should.not.exist(err);
-      inst.toObject().should.eql({ name: 'Wilma' });
-      
+      inst.toObject().should.eql({name: 'Wilma'});
+
       Person.count(function(err, count) {
         should.not.exist(err);
         count.should.equal(0);
@@ -40,18 +36,18 @@ describe('Transient connector', function () {
       });
     });
   });
-  
+
   it('should generate a random string id', function(done) {
     should.exist(Widget.definition.properties.id);
     should.exist(Widget.definition.properties.name);
-    
+
     Widget.definition.properties.id.type.should.equal(String);
-    
-    Widget.create({ name: 'Thing' }, function(err, inst) {
+
+    Widget.create({name: 'Thing'}, function(err, inst) {
       should.not.exist(err);
       inst.id.should.match(/^[0-9a-fA-F]{24}$/);
       inst.name.should.equal('Thing');
-      
+
       Widget.findById(inst.id, function(err, widget) {
         should.not.exist(err);
         should.not.exist(widget);
@@ -59,17 +55,17 @@ describe('Transient connector', function () {
       });
     });
   });
-  
+
   it('should generate a random number id', function(done) {
     should.exist(Item.definition.properties.id);
     should.exist(Item.definition.properties.name);
-    
+
     Item.definition.properties.id.type.should.equal(Number);
-    
-    Item.create({ name: 'Example' }, function(err, inst) {
+
+    Item.create({name: 'Example'}, function(err, inst) {
       should.not.exist(err);
       inst.name.should.equal('Example');
-      
+
       Item.count(function(err, count) {
         should.not.exist(err);
         count.should.equal(0);
@@ -77,5 +73,4 @@ describe('Transient connector', function () {
       });
     });
   });
-  
 });
