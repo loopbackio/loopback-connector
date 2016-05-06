958c6619e3353c4604adc15161fa3a39c8767dfd
Ryan Graham <r.m.graham@gmail.com>
2016-05-05 21:50:59 -0700

    update copyright notices and license

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2014,2016. All Rights Reserved.
+// Node module: loopback-connector
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 exports.Connector = require('./lib/connector');
 // Set up SqlConnector as an alias to SQLConnector
 exports.SQLConnector = exports.SqlConnector = require('./lib/sql');
--- test/sql.test.js
+++ test/sql.test.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2015,2016. All Rights Reserved.
+// Node module: loopback-connector
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 var expect = require('chai').expect;
 var SQLConnector = require('../lib/sql');
 var ParameterizedSQL = SQLConnector.ParameterizedSQL;
